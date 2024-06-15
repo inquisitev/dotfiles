2c28c2ba859d2232c9f2fd4acfb58ab036d831b5
Trevor Keegan <positev@outlook.com>
2024-06-15 17:03:10 -0500

    fix windows

--- .chezmoiscripts/run_once_before_apply_windows.ps1
+++ .chezmoiscripts/run_once_before_apply_windows.ps1
@@ -1,6 +1,18 @@
 # Windows specific configuration
 if ($env:OS -eq "Windows_NT") {
-  # Move nvim config to the correct location
-  Move-Item -Path "$env:LOCALAPPDATA\nvim" -Destination "$env:LOCALAPPDATA\nvim_backup_windows" -Force
-  chezmoi apply --source-path ~/.local/share/chezmoi/dot_config/nvim_windows $env:LOCALAPPDATA\nvim
+  # Define the nvim configuration path
+  $nvimConfigPath = "$env:LOCALAPPDATA\nvim"
+
+  # Create the directory if it doesn't exist
+  if (-Not (Test-Path -Path $nvimConfigPath)) {
+    New-Item -ItemType Directory -Path $nvimConfigPath
+  }
+
+  # Backup existing nvim configuration if it exists
+  if (Test-Path -Path $nvimConfigPath) {
+    Move-Item -Path $nvimConfigPath -Destination "$nvimConfigPath_backup_windows" -Force
+  }
+
+  # Apply the chezmoi nvim configuration
+  chezmoi apply --source-path ~/.local/share/chezmoi/dot_config/nvim_windows -destination $nvimConfigPath
 }
