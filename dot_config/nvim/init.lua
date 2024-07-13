2727dc9db362b0f0b21e926d2eacebe9d8378898
Trevor Keegan <positev@outlook.com>
2024-07-12 20:30:36 -0500

    Add shortcut for telescope

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -40,7 +40,9 @@ require("lazy").setup({
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    { "<leader>tf", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+    { "<leader>tc", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" }
   }
 },
 
