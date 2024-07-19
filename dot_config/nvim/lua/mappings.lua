b7a5e12ae0681cf1a6cf8c332c893f3706ce24e7
Trevor Keegan <KeeganTrevor@JohnDeere.com>
2024-07-19 15:46:05 -0500

    make float termwider

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -8,3 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map({ "n", "t" }, "<A-i>", function()
+    require("nvchad.term").toggle { pos = "float", id = "floatTerm", float_opts={
+        row = 0.35,
+        col = 0.05,
+        width = 0.9,
+        height = 0.5
+    }}
+end, { desc = "terminal toggle floating term" })
