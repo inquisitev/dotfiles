8a83ff57bbaf56e0ea377622e4c458036f591097
Trevor Keegan <KeeganTrevor@JohnDeere.com>
2024-11-12 08:01:45 -0600

    dont update if not on jdnet

--- run_once.sh
+++ run_once.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ "$(hostname -f)" == *jdnet.deere.com* ]]; then
+if [[ "$(hostname -f)" != *jdnet.deere.com* ]]; then
    (
       cd ~/.local/share/chezmoi
-      git submodule add --force git@github.deere.com:l6ti0g0/private-dotfiles.git private-dotfiles
+      git config --local submodule.private-dotfiles.update none
    )
 fi
 
