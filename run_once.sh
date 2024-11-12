5e14313feb6c1624436da215e3f48ddb989d7931
Trevor Keegan <KeeganTrevor@JohnDeere.com>
2024-11-12 08:33:42 -0600

    rest of templates?

--- run_once.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$(hostname -f)" != *jdnet.deere.com* ]]; then
-   (
-      cd ~/.local/share/chezmoi
-      git config --local submodule.private-dotfiles.update none
-   )
-fi
-
