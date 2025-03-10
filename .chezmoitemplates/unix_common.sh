b03b16a5320f8351d85ec5c7d1111b9edb533897
Trevor Keegan <tkgn15@live.com>
2025-03-10 08:58:51 -0500

    updates for rpi

--- .chezmoitemplates/unix_common.sh
+++ .chezmoitemplates/unix_common.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 sudo apt install python-is-python3
 sudo apt install python3-pip
 sudo apt install pipx
@@ -20,18 +18,4 @@ sudo snap install go --classic
 go install github.com/lemonade-command/lemonade@latest
 
 flatpak install flathub org.wezfurlong.wezterm
-
-
-{{ if eq .chezmoi.os "linux" -}}
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-{{ end -}}
-
-{{ if eq .chezmoi.os "darwin" -}}
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
-tar xzf nvim-macos-arm64.tar.gz
-./nvim-macos-arm64/bin/nvim
-{{ end -}}
-
 sudo snap install bw 
