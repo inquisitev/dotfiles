2f5a251a855ebbf7ce8ad6e19c013500a06a34e7
Trevor Keegan <positev@outlook.com>
2025-03-30 20:56:13 -0500

    remove the ugly completions

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -34,26 +34,9 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-      vim.diagnostic.config({
-         virtual_lines = { current_line = true },
-         virtual_text = false,
-         float = {
-          border = "rounded",
-          source = "always",
-        }
-      })
-    end
-  end,
-})
-
 vim.lsp.enable({'clangd', 'pyright', 'lua-lsp-server', 'rust-analyzer', 'qmlls', 'anakinls', 'lua-language-server' })
 
-vim.cmd("set completeopt+=noselect")
+-- vim.cmd("set completeopt+=noselect")
 
 require "nvchad.autocmds"
 vim.schedule(function() require "mappings" end)
