d1e320f735efb5c311900fcfd22f5271141bdcfc
Trevor Keegan <positev@outlook.com>
2025-04-04 21:07:21 -0500

    clangd switch header source working

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -28,4 +28,28 @@ map("n", "<A-o>", "<cmd>copen<CR>", {desc = "quickfix open"})
 map("n", "<A-x>", "<cmd>cclose<CR>", {desc = "quickfix close"})
 
 
--- map("n", "<C-m>", "<cmd>cclose<CR>")
+local util = require 'lspconfig.util'
+
+local function switch_source_header(bufnr)
+  local method_name = 'textDocument/switchSourceHeader'
+  bufnr = 0
+  -- bufnr = util.validate_bufnr(bufnr)
+  local client = util.get_active_client_by_name(bufnr, 'clangd')
+  if not client then
+    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+  end
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+  client.request(method_name, params, function(err, result)
+    if err then
+      error(tostring(err))
+    end
+    if not result then
+      vim.notify('corresponding file cannot be determined')
+      return
+    end
+    vim.cmd.edit(vim.uri_to_fname(result))
+  end, bufnr)
+end
+
+  vim.keymap.set("n", "gs", switch_source_header)
+
